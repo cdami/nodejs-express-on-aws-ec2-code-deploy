326dfeca03777add9f5a81aebef7701eaefc4c75
Calin Damian <calin@temperfield.com>
2022-04-14 17:51:29 +0300

    am pus pe Emil & Ana

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success Yeees</h4> <p>Version 5.3</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success Yeees Cu Emil si Ana :-)</h4> <p>Version 5.4</p>');
 })
 
 app.get('/products', (req, res) => {
@@ -19,7 +19,6 @@ app.get('/products', (req, res) => {
   ])
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Demo app is up and listening to port: ${port}`);
 })
- 
